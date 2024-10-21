f66bdc011a4d82cf538fbc6a9ee92cbd14518871
Taufiq Ridwan <taufiq26.ridwan@gmail.com>
2024-10-21 16:27:35 +0700

    get and post API

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './profile.css';
 import './gallery.css';
 
 function Profile() {
+  const [publisher, setPublisher] = useState('Gwetaufiq');
   const [followers, setFollowers] = useState(0);
   const [following, setFollowing] = useState(0);
   const [posts, setPosts] = useState(0);
+  const [postsData, setPostsData] = useState([]);
+
+  const [imageTitle, setImageTitle] = useState('');
+  const [imageDescription, setImageDescription] = useState('');
+  const [imageLink, setImageLink] = useState('');
+
+  const getPostsData = () => {
+    axios.get('https://gallery-api.baradeveloper.com/public/posts')
+      .then(response => {
+        setPostsData(response.data.posts);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  };
+
+  const submitImage = () => {
+    const newPost = {
+      title: imageTitle,
+      description: imageDescription,
+      image_link: imageLink,
+      publisher: publisher,
+    };
+    axios.post('https://gallery-api.baradeveloper.com/public/post', newPost, {
+      headers: {
+        'Content-Type': 'application/json'
+      }})
+      .then(response => {
+        alert('Image submitted successfully!');
+        getPostsData();
+      })
+      .catch(error => {
+        console.error('Error submitting image:', error);
+      });
+  }
+
+  useEffect(() => {
+    getPostsData();
+  }, []);
 
   return (
     <>
       <div className="profile-page">
         <div className="profile-header">
-          <h1>John Doe</h1>
+          <h1>{publisher}</h1>
           <p>Software Engineer</p>
         </div>
         <div className="profile-picture-container">
@@ -55,99 +96,40 @@ function Profile() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
           </div>
         </div>
+        
+        <div className="container mb-4">
+          <div className="row">
+            <div className="col-4">
+              <label htmlFor="title">Judul:</label>
+              <input type="text" id="title" placeholder="Judul gambar" className="form-control" onChange={(e) => setImageTitle(e.target.value)} />
+            </div>
+            <div className="col-4">
+              <label htmlFor="description">Deskripsi:</label>
+              <input type="text" id="description" placeholder="Deskripsi gambar" className="form-control" onChange={(e) => setImageDescription(e.target.value)} />
+            </div>
+            <div className="col-4">
+              <label htmlFor="image_link">Link Gambar:</label>
+              <input type="text" id="image_link" placeholder="Link gambar" className="form-control" onChange={(e) => setImageLink(e.target.value)} />
+            </div>
+          </div>
+          <button className="btn btn-primary mt-3 w-100" onClick={() => submitImage()}>Submit</button>
+        </div>
+
         <Link className="btn btn-secondary" to="/">
           {"Go Back"}
         </Link>
       </div>
 
-      <div class="pinterest-grid m-5">
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/474x/90/7e/6c/907e6c32258c0b593844a8fec7bac6ef.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
+      <div className="pinterest-grid m-5">
+        {postsData.map((post, index) => (
+          <div className="pinterest-item" key={index}>
+            <img src={post.image_link} alt={`Image ${post.title} ${index + 1}`} />
+            <div className="title">{post.title}</div>
+            <div className="author">{post.publisher}</div>
           </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/ac/bb/1a/acbb1a45ca75704eebc1f5a4aa5b6da1.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/53/bf/b0/53bfb0bfe399636ffd402cd17f50247a.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/474x/99/a4/5f/99a45fd961c35f5a5560d107d6ea96c0.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/1c/20/7c/1c207c7be2861d33f75fba2126d245ca.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/65/5b/9e/655b9ec3a86ea71b9eb4f2a0da0a1661.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/7b/e8/2d/7be82d991425fbc5cdef63e2c27de674.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/83/c0/c0/83c0c0ad657695a3053cff3da19d42e3.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/e7/ea/7d/e7ea7d4df756d8e783247687ff64f0cd.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/474x/1f/13/8a/1f138a6e79e9693ca43f7f75f96d43e2.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/474x/e4/96/63/e49663623792148f3554a38b9cc97a11.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
-        <div class="pinterest-item">
-          <img src="https://i.pinimg.com/enabled/474x/1a/46/cd/1a46cd40d473a74a1aa329b98c1bac2c.jpg" alt="Image 1" />
-          <div class="title">Warrior & His Robot</div>
-          <div class="author">
-            Hawthorne Entertainment
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
 }
-
 export default Profile;
